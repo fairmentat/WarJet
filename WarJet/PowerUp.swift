d9e695d7235db3d0071fca56bd42c0809cd5d2c7
fairmentat <fairmentat@mail.ru>
2020-05-05 12:51:32 +0400

    add powerUP missile animation

--- WarJet/PowerUp.swift
+++ WarJet/PowerUp.swift
@@ -16,9 +16,10 @@ class PowerUp: SKSpriteNode {
     var animationSpriteArray = [SKTexture]()
     
     init() {
-        let greenTexture = textureAtlas.textureNamed("Missile 01")
-        super.init(texture: <#T##SKTexture?#>, color: .clear, size: initialSize)
+        let greenTexture = textureAtlas.textureNamed("Missile_01")
+        super.init(texture: greenTexture, color: .clear, size: initialSize)
         self.name = "powerUp"
+        self.zPosition = 20
         
     }
     
@@ -30,10 +31,10 @@ class PowerUp: SKSpriteNode {
         for i in 1...16 {
             
             let number = String(format: "%02d", i)
-            animationSpriteArray.append(SKTexture(imageNamed: "Missile \(number)"))
+            animationSpriteArray.append(SKTexture(imageNamed: "Missile_\(number)"))
         }
         SKTexture.preload(animationSpriteArray) {
-            let rotation = SKAction.animate(with: self.animationSpriteArray, timePerFrame: 0.5, resize: true, restore: false)
+            let rotation = SKAction.animate(with: self.animationSpriteArray, timePerFrame: 0.2, resize: true, restore: false)
             let rotationForever = SKAction.repeatForever(rotation)
             self.run(rotationForever)
         }
